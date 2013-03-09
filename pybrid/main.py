4e84ea7e7cfda60ccbcca6a9f379cce78d0b2b89
Paul H <git@heymann.be>
2013-03-09 12:23:40 +0000

    add initial version of main opt parsing

--- pybrid/main.py
+++ pybrid/main.py
@@ -11,6 +11,8 @@
 """
 
 import pybrid
+import optparse
+import sys
 
 def run(top_level_dir, *args, **kwargs):
     found_reports = pybrid.defaultReportLoader.discover(top_level_dir)
@@ -19,3 +21,92 @@ def run(top_level_dir, *args, **kwargs):
 
     return r
 
+def main(top_level_dir, *args, **kwargs):
+    parser = optparse.OptionParser()
+
+    # actions:
+    parser.add_option('-r', '--run',
+                      action="store_true", dest="run", default=False,
+                      help="generate reports")
+    parser.add_option('-l', '--list',
+                      action="store_true", dest="list", default=False,
+                      help="list reports")
+
+    # params:
+    parser.add_option("-d", "--output_dir", dest="output_dir",
+                      help="write reports to DIR", metavar="DIR")
+    parser.add_option("-e", "--executable",
+                      help="execute command CMD with report HTML index",
+                      metavar="CMD", dest='executable')
+
+    # filters
+    parser.add_option("-g", "--group",
+                      action="append", dest="include_groups",
+                      help="generate reports in this GROUP (may be repeated)")
+    parser.add_option("-a", "--author",
+                      help="generate reports by this AUTHOR",
+                      metavar="AUTHOR", dest='author')
+    parser.add_option("-n", "--name",
+                      help="generate report with this NAME",
+                      metavar="NAME", dest='name')
+
+    (options, args) = parser.parse_args()
+
+    if (not (options.run or options.list) or
+        (options.run and options.list)):
+        print >>sys.stderr, 'Must either list or run reports.'
+        parser.print_help()
+        sys.exit(1)
+
+    if options.run and not options.output_dir:
+        print >>sys.stderr, 'Output dir is required for running reports.'
+        parser.print_help()
+        sys.exit(1)
+        
+    if options.list:
+        found_reports = pybrid.defaultReportLoader.discover(top_level_dir)
+        reports = []
+        for found_report in found_reports:
+            inst = found_report()
+            reports.append((inst.get_name(),
+                            inst.get_author(),
+                            inst.get_groups(),
+                            str(found_report)))
+
+        for name, author, groups, cls_s in list(sorted(reports)):
+            print name, author, groups, cls_s
+
+    if options.run:
+        if not kwargs.has_key('pre_hooks'):
+            kwargs['pre_hooks'] = []
+
+        if not kwargs.has_key('report_filters'):
+            kwargs['report_filters'] = []
+
+        if not kwargs.has_key('post_hooks'):
+            kwargs['post_hooks'] = []
+
+        if options.include_groups:
+            kwargs['report_filters'].append(
+                pybrid.filters.GroupFilter(options.include_groups)
+                )
+
+        if options.author:
+            kwargs['report_filters'].append(
+                pybrid.filters.AuthorFilter(options.author))
+
+        if options.name:
+            kwargs['report_filters'].append(
+                pybrid.filters.NameFilter(options.name))
+
+        if options.executable:
+            kwargs['post_hooks'].append(
+                pybrid.hooks.ExecuteCommandOnIndexHook(options.executable)
+                )
+        
+        found_reports = pybrid.defaultReportLoader.discover(top_level_dir)
+        r = pybrid.ReportRunner(
+            found_reports, options.output_dir, *args, **kwargs)
+        r.run()
+        
+        return r
