20b200206749a6b4cc0deb0784ea781ce18dad02
Paul H <git@heymann.be>
2013-03-09 12:32:17 +0000

    add necessary import

--- pybrid/__init__.py
+++ pybrid/__init__.py
@@ -1,6 +1,6 @@
 from loader import defaultReportLoader
 from report import PybridReport
 from runner import ReportRunner, author_name_report_dir_mapping
-from main import run
+from main import run, main
 import filters
 import hooks
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pybrid',
-    version='1.0.0',
+    version='1.0.1',
     author='Paul Heymann',
     author_email='pybrid@heymann.be',
     packages=['pybrid', ],
